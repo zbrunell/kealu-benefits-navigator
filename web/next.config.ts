dd5a761af7e49bc4aff092cce78ce7d11f25aba2
Zachary Brunell <zbrunell@gmail.com>
2026-06-26 15:22:24 -0500

    Updated next.config.ts added experimental.

--- web/next.config.ts
+++ web/next.config.ts
@@ -7,7 +7,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  experimental: {
 
+    instrumentationHook: true,
+
+  },
   async headers() {
     return [
       {
